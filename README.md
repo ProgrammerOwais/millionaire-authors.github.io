8053a8b01eeed5d70beab70dd93f2e0e8635bb01
ProgrammerOwais <muhammadowaiskhan616@gmail.com>
2021-07-11 12:21:16 +0500

    commit for pushing

---
+++ README.md
@@ -0,0 +1,42 @@
+### links
+live site link :
+
+
+# About Project
+
+This project is about millionaire authors. This web includes all important info
+about millionaire authors. This provides basic information that a simple authors 
+become millionaires what are the secrets behind theire success.
+
+
+## Materials
+.This project is made up of bootstrap library 
+.little bit my own css
+.my own javascript
+
+
+
+## Designing
+
+This web includes most attractive and simple designing.
+
+In this project you will go along with different images,
+colors , fonts and text.
+
+In this project I also included some basic bootstrap 
+modals , links and forms with beautiful layout.
+
+## Coding
+
+I also included some coding of JavaScript.
+You will find my coding effect at top of nav bar in about section
+by clicking it.
+
+
+## Tune ON
+
+Be awake more project are coming with most 
+beautiful, attractive  and modern desingning
+with simple and secure javascript coding strategies.
+
+### Thank you for Visiting in here.
---
+++ bootstrappractice.js
@@ -0,0 +1,64 @@
+var div1        = document.querySelector(".demo1");
+var div2        = document.querySelector(".demo2");
+var div3        = div1.innerText;
+var h1          = document.querySelector('.about-heading1');
+var h2          = document.querySelector('.about-heading2');
+var h3          = h1.innerText;
+var nodeText1   = document.createTextNode;
+var nodeText2   = document.createTextNode;
+var heading     =[];
+var span       = [];
+
+nodeText1 = h3;
+nodeText2 = div3;
+ // heading section
+
+setTimeout(() => {
+
+    for (let i=0; i<nodeText1.length; i++) {
+        heading[i] = document.createElement('span');
+        heading[i].innerHTML = nodeText1[i];
+        heading[i].style.opacity='0';
+        h2.appendChild(heading[i]);
+       }
+    var k= 0;
+    var t1 = setInterval(() => {
+    
+        heading[k].style.opacity="1";
+        k=k+1;
+    
+        if(k===heading.length) {
+            clearInterval(t1);
+        }
+        
+    }, 100);
+     
+ },500);
+setTimeout(() => {
+    for (let i=0; i<nodeText2.length; i++) {
+        span[i] = document.createElement('span');
+        span[i].innerHTML = nodeText2[i];
+        span[i].style.opacity='0';
+        div2.appendChild(span[i]);
+    
+       }
+    
+    var j= 0;
+    
+    var t2 = setInterval(() => {
+    
+        span[j].style.opacity="1";
+        j=j+1;
+    
+        if(j===span.length) {
+            clearInterval(t2);
+        }
+        
+    }, 100);
+    
+}, 4000);
+
+
+
+
+   
